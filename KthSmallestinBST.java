0a4b049dbbcf8d44d52410526d62e4d2af9511bf
Harsh Gupta <hgupta7@umd.edu>
2025-03-05 01:03:24 -0500

    Trees-4 done

---
+++ KthSmallestinBST.java
@@ -0,0 +1,24 @@
+// TC - O(N)
+// SC - O(N)
+class Solution {
+    private static int number = 0;
+    private static int count = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        helper(root);
+        return number;
+    }
+
+    public void helper(TreeNode root) {
+        if (root.left != null)
+            helper(root.left);
+        count--;
+        if (count == 0) {
+            number = root.val;
+            return;
+        }
+        if (root.right != null)
+            helper(root.right);
+    }
+}
